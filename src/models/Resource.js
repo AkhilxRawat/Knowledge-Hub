4429753b3dda000d82d71408c1cd82953b51bce7
Akhil Singh Rawat <114263685+AkhilxRawat@users.noreply.github.com>
2026-05-10 10:47:07 +0530

    Update Resource.js

--- src/models/Resource.js
+++ src/models/Resource.js
@@ -4,7 +4,7 @@ const resourceSchema = new mongoose.Schema(
   {
     user:        { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, index: true },
     title:       { type: String, required: true, trim: true },
-    url:         { type: String, required: true, trim: true },
+    url:         { type: String, required: false, default:null, trim: true },
     description: { type: String, default: null, trim: true },
     tags:        { type: [String], default: [] },
   },
